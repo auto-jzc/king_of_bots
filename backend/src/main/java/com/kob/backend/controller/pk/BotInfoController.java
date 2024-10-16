0ed92c33707a5fa7d6eaf3a6b3665cda70c687e8
jiale ma <1519890525@qq.com>
2024-10-16 12:39:29 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,56 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+//如果想返回数据的话，这里就要用RestController
+//我们这一个类里面所有的链接都是在pk目录下，每一个模块的链接应该都在某一个对应的目录下，pk模块应该都在/pk/里面
+//父目录用RequestMapping,加上一个映射
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    //前端链接对应的就是一个函数名http://localhost:8080/pk/getbotinfo/，链接对应访问那个函数，它是通过RestController一层一层找出来的
+    @RequestMapping("getbotinfo/")
+
+    //想返回一个字符串
+//    public String getBotInfo(){
+//        return "hhhhh";
+//    }
+
+    //返回一个链表
+//    public List<String> getBotInfo(){
+//        List<String> list=new LinkedList<>();
+//        list.add("sword");
+//        list.add("tiger");
+//        list.add("apple");
+//        return list;
+//    }
+
+    //返回一个字典
+//    public Map<String,String> getBotInfo(){
+//        Map<String,String> map=new HashMap<>();
+//        map.put("name","tiger");
+//        map.put("rating","1500");
+//        return map;
+//    }
+
+    //嵌套
+    public List<Map<String,String>> getBotInfo(){
+        List<Map<String,String>> list=new LinkedList<>();
+        Map<String,String> bot1=new HashMap<>();
+        bot1.put("name","tiger");
+        bot1.put("rating","1500");
+        Map<String,String> bot2=new HashMap<>();
+        bot2.put("name","apple");
+        bot2.put("rating","1800");
+        list.add(bot1);
+        list.add(bot2);
+        return list;
+    }
+}
